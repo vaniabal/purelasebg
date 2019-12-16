0a200a99ea54c465c42195b211c1a0e1206674cb
Dari <dari.k@abv.bg>
2019-12-16 17:08:23 +0200

    faq content

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -1,17 +1,43 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 import { Title } from "../utils"
+import QuestionsList from "./QuestionsList"
 import "./faq.css"
 
-import QuestionItem from "./QuestionItem"
+const QUESTIONS = graphql`
+  {
+    questions: allContentfulFaq {
+      nodes {
+        answer
+        question {
+          question
+        }
+        id
+        position
+      }
+    }
+  }
+`
 
 export default function Faq() {
   return (
-    <div className="faq-container">
-      <Title
-        title="често задавани въпроси"
-        subtitle="Всичко което искате да знаете"
-      />
-      <QuestionItem />
-    </div>
+    <section className="faq-section">
+      <div className="container">
+        <div className="row">
+          <div className="faq-container">
+            <Title
+              title="често задавани въпроси"
+              subtitle="Всичко което искате да знаете"
+            />
+            <StaticQuery
+              query={QUESTIONS}
+              render={({ questions: { nodes } }) => (
+                <QuestionsList questions={nodes} />
+              )}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
