81cf991bdfeeffb3ca759ac8c4e29e5942b6bd11
Dari <dari.k@abv.bg>
2019-12-17 11:01:22 +0200

    faq styling

--- src/components/faq/QuestionsList.js
+++ src/components/faq/QuestionsList.js
@@ -1,13 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import QuestionItem from "./QuestionItem"
 import { sortQuestions } from "../utils/sortQuestions"
 
 const QuestionsList = ({ questions }) => {
   const sortedQuestions = sortQuestions(questions)
+  const [openQuestion, setOpenQuestion] = useState(1)
+
+  const toggleQuestion = position => {
+    if (position !== openQuestion) {
+      setOpenQuestion(position)
+    } else {
+      setOpenQuestion(0)
+    }
+  }
+
   return (
     <div>
       {sortedQuestions.map((node, index) => {
-        return <QuestionItem key={node.id} i={index} {...node} />
+        return (
+          <QuestionItem
+            key={node.id}
+            openQuestion={openQuestion}
+            {...node}
+            toggleQuestion={toggleQuestion}
+          />
+        )
       })}
     </div>
   )
